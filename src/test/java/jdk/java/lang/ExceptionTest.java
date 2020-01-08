e22044a294e671285507f7937240a14ca8e6349d
fixalot <fixalot84@gmail.com>
2020-01-08 14:11:02 +0900

    hi

--- src/test/java/jdk/java/lang/ExceptionTest.java
+++ src/test/java/jdk/java/lang/ExceptionTest.java
@@ -21,7 +21,9 @@ public class ExceptionTest {
 			int nan = 1 / 0;
 		} catch (Exception e) {
 //			logger.debug(e.getMessage()); // 이렇게 하면 예외 메시지만 나옴.
+//			logger.debug(e.toString()); // 이렇게 하면 예외 클래스 + 예외 메시지만 나옴.
 			logger.debug(e.getMessage(), e);
+//			logger.debug("", null, null, null, null, null, null, null);
 		}
 		Assert.assertTrue(true);
 	}
