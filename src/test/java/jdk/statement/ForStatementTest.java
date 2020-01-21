434828619ba45632318eb5f169a65c432da35789
fixalot <fixalot84@gmail.com>
2020-01-21 16:30:58 +0900

    add

---
+++ src/test/java/jdk/statement/ForStatementTest.java
@@ -0,0 +1,16 @@
+package jdk.statement;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForStatementTest {
+	private static final Logger logger = LoggerFactory.getLogger(ForStatementTest.class);
+
+	@Test
+	public void test() {
+		for (int i = 0; i != 0; i++) {
+			logger.debug(String.valueOf(i));
+		}
+	}
+}
